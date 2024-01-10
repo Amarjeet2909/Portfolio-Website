81d21c10d9ff11487ba5baf2713971643b0b53a1
Amarjeet2909 <ak4348225@gmail.com>
2024-01-10 20:28:56 +0530

    Latest updated files

--- src/utils/temp.jsx
+++ src/utils/temp.jsx
@@ -1,105 +1,147 @@
-import React from 'react';
-import Navbar from './Navbar';
-import '../components/Styles/achievement.css';
-import AnimatedSection from './AnimatedSection';
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
-import i1 from "../assets/Certificate/i1.png";
-import i2 from "../assets/Certificate/i2.png";
-import i3 from "../assets/Certificate/i3.png";
-import i4 from "../assets/Certificate/i4.png";
-import i5 from "../assets/Certificate/i5.png";
-import i6 from "../assets/Certificate/i6.png";
-import i7 from "../assets/Certificate/i7.png";
-import i8 from "../assets/Certificate/i8.png";
-import i9 from "../assets/Certificate/i9.png";
-import i10 from "../assets/Certificate/i10.png";
-import i11 from "../assets/Certificate/i11.png";
-import i12 from "../assets/Certificate/i12.png";
-import i13 from "../assets/Certificate/i13.png";
-import i14 from "../assets/Certificate/i14.jpg";
-import i15 from "../assets/Certificate/i15.png";
-import i16 from "../assets/Certificate/i16.jpg";
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+import "./Styles/contact.css";
 
-const Achievement = () => {
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-  const courses = [
-    { id: 1, imageSrc: i2, description: 'Certification Description 2' },
-    { id: 2, imageSrc: i3, description: 'Certification Description 1' },
-    { id: 3, imageSrc: i4, description: 'Certification Description 2' },
-    { id: 4, imageSrc: i5, description: 'Certification Description 1' },
-    { id: 5, imageSrc: i15, description: 'Certification Description 1' },
-    { id: 6, imageSrc: i16, description: 'Certification Description 1' },
-  ];
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-  const internship = [
-    { id: 1, imageSrc: i1, description: 'Certification Description 2' },
-    { id: 2, imageSrc: i8, description: 'Certification Description 1' },
-    { id: 3, imageSrc: i14, description: 'Certification Description 2' },
-    { id: 4, imageSrc: i11, description: 'Certification Description 1' },
-    { id: 5, imageSrc: i6, description: 'Certification Description 1' },
-    { id: 6, imageSrc: i13, description: 'Certification Description 1' },
-  ];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_h4vyh4v",
+        "template_nohaukg",
+        {
+          from_name: form.name,
+          to_name: "Amarjeet",
+          from_email: form.email,
+          to_email: "ak4348225@gmail.com",
+          message: form.message,
+        },
+        "eLEXtDqxtTgskyDA8"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+
+          console.log(error);
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
-    <>
-    <Navbar />
+    <div className="video-container">
 
-    <div className="achievement-section">
-
-       <AnimatedSection
-        sectionId="certifications-section"
-        heading="Courses"
-        paragraph="These are few online courses that I have completed on various platforms like Udemy and Linkedin learning"
-        />
-        
-      <div className="certifications">
-        {courses.map((courses) => (
-          <div className="certificate-box" key={courses.id}>
-            <img src={courses.imageSrc} alt={`Certificate ${courses.id}`} className="certificate-image" />
-            <p className="certificate-description">{courses.description}</p>
-          </div>
-        ))}
+      <div className="video-wrapper">
+      <iframe
+      class="youtube-video"
+      width="70%"
+      height="450"
+      src="https://www.youtube-nocookie.com/embed/UpLT6zVF2XY?showinfo=0&controls=0&rel=0"
+      title="YouTube Video"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      sandbox="allow-same-origin allow-scripts"
+      ></iframe>
       </div>
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch with me</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="Write your message"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="Write your message"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.6, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[300px]"
+      >
+        <EarthCanvas />
+      </motion.div>
     </div>
-
-    <div className="achievement-section">
-
-      <AnimatedSection
-      sectionId="internship-section"
-      heading="Internship | Volunteering"
-      paragraph="These are few online courses that I have completed on various platforms like Udemy and Linkedin learning"
-      />
-      
-      <div className="certifications">
-      {internship.map((internship) => (
-        <div className="certificate-box" key={internship.id}>
-          <img src={internship.imageSrc} alt={`Certificate ${internship.id}`} className="certificate-image" />
-          <p className="certificate-description">{internship.description}</p>
-        </div>
-      ))}
-      </div>
-      </div>
-
-      <div className="achievement-section">
-
-    <AnimatedSection
-    sectionId="internship-section"
-    heading="Internship | Volunteering"
-    paragraph="These are few online courses that I have completed on various platforms like Udemy and Linkedin learning"
-    />
-
-    <div className="certifications">
-    {internship.map((internship) => (
-      <div className="certificate-box" key={internship.id}>
-        <img src={internship.imageSrc} alt={`Certificate ${internship.id}`} className="certificate-image" />
-        <p className="certificate-description">{internship.description}</p>
-      </div>
-    ))}
     </div>
-    </div>
-
-    </>
   );
 };
 
-export default Achievement;
+export default SectionWrapper(Contact, "contact");
